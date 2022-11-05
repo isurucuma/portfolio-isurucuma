06898b9e3f09e1415ff743ee5a600dcf20517a22
isuru <isurucuma@gmail.com>
2022-11-05 22:28:08 +0530

    test

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,6 +1,7 @@
 import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async function () {
+  console.log(`${process.env.NEXT_PUBLIC_BASE_URL}api/getPageInfo`);
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getPageInfo`);
   const data = await res.json();
 
